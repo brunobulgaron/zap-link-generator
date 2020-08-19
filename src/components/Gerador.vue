672017f3fd1310543c86ffb0a95a2354e783ceef
Bruno Bulgaron <brunohbulgaron@gmail.com>
2020-08-19 00:13:05 -0300

    First working version

---
+++ src/components/Gerador.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="col-12 py-5">
+    <div class="form-group">
+      <label for="">Número</label>
+      <input type="text" v-model="numero" class="form-control">
+    </div>
+    <div class="form-group">
+      <label for="">Mensagem</label>
+      <input type="text" v-model="mensagem" class="form-control">
+    </div>
+    <button @click="geraLink()" class="btn btn-primary btn-lg btn-block">Gerar Link</button>
+  {{url}}
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Gerador",
+  data: function() {
+    return {
+      numero: "",
+      mensagem: "",
+      url: "",
+    };
+  },
+  methods: {
+    geraLink: function() {
+      let n = this.numero.replace(/[^0-9]/g, '');
+      let m = encodeURI(this.mensagem)
+      this.url = `https://wa.me/send?phone=${n}&text=${m}`;
+      this.$router.push({ name: 'Resultado', params: {
+        link: this.url
+      } });
+    }
+  }
+};
+</script>
---
+++ src/views/Resultado.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-12">
+        <div class="form-group">
+          <label for="example">Seu link foi gerado</label>
+          <textarea
+            class="form-control"
+            id="url"
+            rows="3"
+            v-model="$route.params.link"
+          ></textarea>
+        </div>
+      </div>
+      <div class="col-12 d-flex justify-content-around">
+        <button
+          id="copiar"
+          data-clipboard-target="#url"
+          class="btn btn-success"
+        >
+          Copiar
+        </button>
+        <a target="_blank" :href="$route.params.link" class="btn btn-warning"
+          >Testar</a
+        >
+        <router-link to="/" class="btn btn-danger">Gerar Novo Link</router-link>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import ClipboardJS from "clipboard";
+new ClipboardJS("#copiar");
+
+export default {};
+</script>
